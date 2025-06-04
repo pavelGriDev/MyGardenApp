b95a5593c7c16083dbcf92df6a8e99d13c8b1f9e
Pavel Gritskov <pavel_ios_dev@mobilexapps.com>
2025-06-04 23:58:00 +0400

    Initial Commit

---
+++ MyGardenApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyGardenApp
+//
+//  Created by Pavel Gritskov on 04.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyGardenApp/MyGardenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGardenAppApp.swift
+//  MyGardenApp
+//
+//  Created by Pavel Gritskov on 04.06.25.
+//
+
+import SwiftUI
+
+@main
+struct MyGardenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
